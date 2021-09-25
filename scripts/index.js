710d1f0fe6d4d00d7d78d4c9c6be70ab711b13b7
Dhruva S <dhruva.shashi@gmail.com>
2021-09-25 17:35:37 +0530

    folder sort

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,4 @@
-function hello() {
-	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.open("GET", 'https://dhruva-shashi.github.io/uvce-hashcode-hub/data.json', false );
-	xmlHttp.send(null);
-
-	var data = JSON.parse(xmlHttp.responseText)
-
-	alert(data[0]['problem-name']);
-}
-
-function buildTable() {
+function build_index() {
 	var xmlHttp = new XMLHttpRequest();
 	xmlHttp.open("GET", 'https://dhruva-shashi.github.io/uvce-hashcode-hub/data.json', false);
 	xmlHttp.send(null);
@@ -16,9 +6,10 @@ function buildTable() {
 	var data = JSON.parse(xmlHttp.responseText);
 
 	card = document.getElementById('content');
+	card.innerHTML = "";
 
 	for (var i = 0; i < data.length; i++) {
-		card_container = `
+		card.innerHTML += `
 			<div class = "card-container">
 				<div class = "card">
 					<div class = "card-header">
@@ -35,15 +26,13 @@ function buildTable() {
 					</div>
 
 					<div class = "button-container">
-						<a href = "problems/${data[i].id}/${data[i].id}.html">
+						<a href = "problems/${data[i].id}.html">
 							<input type = "button" value = "Solve Problem">
 						</a>
 					</div>
 				</div>
 			</div>
 		`;
-
-		card.innerHTML += card_container;
 	}
 }
 
