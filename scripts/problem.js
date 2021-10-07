55ec68902713784800484171be233d619962c6a0
Dhruva S <dhruva.shashi@gmail.com>
2021-10-08 00:15:51 +0530

    cookie commit

--- scripts/problem.js
+++ scripts/problem.js
@@ -198,6 +198,7 @@ function build_problem(id) {
 
 	function readCookie() {
 		var s = document.cookie.split('=')[1].split(' ');
+		alert(s);
 
 		for (i = 0; i < parseInt(data['number-files']); i++) {
 			const best = document.getElementById(`best-score-${String.fromCharCode(97+i)}`);
