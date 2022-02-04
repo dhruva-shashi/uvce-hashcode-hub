7f411a952f19d54d320a416e57d00a49ced4a6aa
Dhruva S <dhruva.shashi@gmail.com>
2022-02-04 17:02:10 +0530

    even more pizza forst commit

--- scripts/even-more-pizza.js
+++ scripts/even-more-pizza.js
@@ -1,25 +1,35 @@
 function evaluate(input, output) {
     var deliveries = parseInt(output[0]);
 
-    var i, j, k, result = 0;
+    var i, j, k, result = 0, n = parseInt(input[0]);
 
     for (i = 0; i < input.length; i++)
         input[i] = input[i].split(' ');
 
-    alert(input);
+    for (i = 0; i < output.length; i++)
+        output[i] = output[i].split(' ');
+        
+    var delivered = new Set();
 
     for (i = 1; i <= deliveries; i++) {
-        var temp = output[i].split(' ');
+        var pizzas = parseInt(output[i][0]);
 
-        alert(temp);
+        var mm = new Set();
 
-        var mm = new Set([]);
+        for (j = 1; j <= pizzas; j++) {
+            var pn = parseInt(output[i][j]);
+            var ts = parseInt(input[pn+1][0]);
 
-        for (j = 1; j <= parseInt(temp[0]); j++) {
-            alert(input[parseInt(temp[j]+1)]);
+            if (delivered.has(pn) || pn < 0 || pn >= n)
+                return 0;
+
+            delivered.add(pn);
+
+            for (k = 1; k <= ts; k++)
+                mm.add(input[pn+1][k]);
         }
 
-        result += (mm.size*mm.size);
+        result += mm.size*mm.size;
     }
 
     return result;
